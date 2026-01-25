b38bd7159b0678538a22a22a0d5ad8354e797ffc
JOSUN_HK <dennistai.mail@gmail.com>
2026-01-26 01:33:04 +0800

    fixed default value for animation DESc

--- db/schema/migration/20260123163302_animations.sql
+++ db/schema/migration/20260123163302_animations.sql
@@ -3,7 +3,7 @@
 CREATE TABLE animation(
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     label varchar(150) character set utf8 NOT NULL,
-    animation_desc varchar(150) character set utf8 NOT NULL,
+    animation_desc varchar(150) character set utf8 NOT NULL DEFAULT '',
     width INT,
     height INT,
     frames_count INT,
